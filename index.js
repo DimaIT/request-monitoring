9f1e48c31d4d6ff4da9cf5679a6bd9f5fc2faa73
dimait <dima@surfly.com>
2022-07-27 21:14:39 +0200

    separate audio handling; ticketswap monitoring

--- index.js
+++ index.js
@@ -1,89 +1,49 @@
-'use strict'
+import fetch from 'node-fetch';
+import { play } from "./audio.js";
 
-/**
- * Code adapted from:
- * http://blogs.msdn.com/b/dawate/archive/2009/06/24/intro-to-audio-programming-part-3-synthesizing-simple-wave-audio-using-c.aspx
- */
-
-const Readable = require('stream').Readable;
-const bufferAlloc = require('buffer-alloc');
-const Speaker = require('speaker');
-
-function play() {
-// the frequency to play
-    const freq = 440.0; // Concert A, default tone
-
-// seconds worth of audio data to generate before emitting "end"
-    const duration = 1.5;
-
-    console.log('generating a %dhz sine wave for %d seconds', freq, duration);
-
-// A SineWaveGenerator readable stream
-    const sine = new Readable();
-    sine.bitDepth = 16;
-    sine.channels = 2;
-    sine.sampleRate = 44100;
-    sine.samplesGenerated = 0;
-    sine._read = read;
-
-// create a SineWaveGenerator instance and pipe it to the speaker
-    sine.pipe(new Speaker());
-
-// the Readable "_read()" callback function
-    function read(n) {
-        const sampleSize = this.bitDepth / 8
-        const blockAlign = sampleSize * this.channels
-        const numSamples = n / blockAlign | 0
-        const buf = bufferAlloc(numSamples * blockAlign)
-        const amplitude = 32760 // Max amplitude for 16-bit audio
-
-        // the "angle" used in the function, adjusted for the number of
-        // channels and sample rate. This value is like the period of the wave.
-        const t = (Math.PI * 2 * freq) / this.sampleRate
-
-        for (let i = 0; i < numSamples; i++) {
-            // fill with a simple sine wave at max amplitude
-            for (let channel = 0; channel < this.channels; channel++) {
-                const s = this.samplesGenerated + i
-                const val = Math.round(amplitude * Math.sin(t * s)) // sine wave
-                const offset = (i * sampleSize * this.channels) + (channel * sampleSize)
-                buf[`writeInt${this.bitDepth}LE`](val, offset)
-            }
-        }
-
-        this.push(buf)
-
-        this.samplesGenerated += numSamples
-        if (this.samplesGenerated >= this.sampleRate * duration) {
-            // after generating "duration" second of audio, emit "end"
-            this.push(null)
-        }
-    }
-}
-
-const fetch = require('node-fetch');
+let lastStatus = null;
 
 async function monitor() {
-    const resp = await fetch("https://oap.ind.nl/oap/api/desks/AM/slots/?productKey=DOC&persons=1");
+    const resp = await fetch("https://www.ticketswap.com/rammstein-ostend-4-augustus-2022", {
+        "credentials": "omit",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/103.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1"
+        },
+        "method": "GET",
+        "mode": "cors"
+    });
 
-    let rawData = await resp.text();
-    if (rawData.indexOf('{') !== 0) {
-        rawData = rawData.substring(rawData.indexOf('{'));
-    }
-    const slots = JSON.parse(rawData).data;
-    const dates = slots.map(item => new Date(`${item.date} ${item.startTime}`));
+    const rawData = await resp.text();
+    const count = rawData.match(/>0<\//g)?.length;
 
-    const smallest = new Date(Math.min(...dates));
-
-    if (smallest > new Date('2022-03-25')) {
-        console.log('\n\nSlot available!!!!', smallest);
+    if (typeof count !== 'number') {
+        console.log(rawData)
+        console.log('\nUnexpected data, abort');
+        process.exit();
+    } else if (count < 4) {
+        console.log('\n\nSlot available!!!!', count);
+        console.log("https://www.ticketswap.com/rammstein-ostend-4-augustus-2022");
         play();
-    } else {
-        console.log('No results,', smallest);
+    }
+
+    const matches = [...rawData.matchAll(/(\d+)<\/span><h6[^>]*>([^<]*)<\/h6>/g)];
+    const status = matches.map(match => `${match[1]} - ${match[2]}`).join('; ');
+
+    if (status !== lastStatus) {
+        console.log(new Date(), status);
+        lastStatus = status;
     }
 }
+
 monitor();
 
 setInterval(async () => {
     await monitor();
-}, 120000);
+}, 30000);
